2042d8fa0d5e50d4bf1bfa24676aaa41aca03e74
bzdvdn <bzdv.dn@gmail.com>
2018-07-01 10:26:30 +0300

    finish parser class

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
 import asyncio
-from aiohttp import request
+from aiohttp import ClientSession
 
 import csv
 from bs4 import BeautifulSoup
@@ -17,22 +17,24 @@ class Parser(object):
 		self.chat_id = chat_id
 
 
+	async def get_fetch(self, client, url):
+		async with client.get(url, skip_auto_headers={"User-Agent": useragent}) as r:
+			return await r.text() 
+
 	async def get_html(self, url, useragent=None):
-		async with request('get', url, skip_auto_headers={"User-Agent": useragent}) as r:
-			html = await r.text()
+		async with ClientSession() as client:
+			html = await self.get_fetch(client, url)
 			return html
 
 
 	async def get_total_pages(self, html):
 		soup = BeautifulSoup(html, "lxml")
 		pages = soup.find('ul', class_='pagination' ).find_all('a')[-2].get('href')
-		print(pages)
-		total_pages = int(pages.split('=')[1].split('&')[0]) + 1
+		total_pages = int(pages.split('=')[-1])
 
 		return int(total_pages)
 
 	async def write_csv(self, data, message, fileprefix):
-		print("WRITE_CSV")
 		with open(str(self.chat_id) + '_-_' + str(self.message) + '.csv', 'a') as f:
 			writer = csv.writer(f)
 			writer.writerow((
@@ -50,17 +52,18 @@ class Parser(object):
 		raise NotImplementedError
 	
 
-	async def workparse(self, useragent):
+	async def start_parsing(self, useragent):
 		print("STARTWORK")
 		# url = 'https://www.work.ua/jobs-' + self.message + '/'
 		# base_url = 'https://www.work.ua/jobs-' + self.message + '/?'
 		# page = 'page='
-		html = await self.get_html(self.url)
-		total_pages = await self.get_total_pages(html)		
+		html = await asyncio.ensure_future(self.get_html(self.url))
+		total_pages = await self.get_total_pages(html)	
 		tasks = []
-		for i in range(1, total_pages+1):
+		for i in range(1, total_pages):
+			print("PAGE: {}".format(i))
 			url_gen = self.base_url + self.page + str(i)
-			html = await self.get_html(url_gen, useragent)
+			html = await asyncio.ensure_future(self.get_html(url_gen, useragent))
 			task = asyncio.ensure_future(self.get_pages_data(html))
 			tasks.append(task)
 
@@ -71,12 +74,10 @@ class WorkUaParser(Parser):
 		soup = BeautifulSoup(html, "lxml")
 		pages = soup.find('ul', class_='pagination' ).find_all('a')[-2].get('href')
 		total_pages = int(pages.split('=')[-1])
-		print(total_pages)
 
 		return int(total_pages)
 
 	async def get_pages_data(self, html):
-		print("DATA")
 		soup = BeautifulSoup(html, "lxml")
 		ads = soup.find_all('div', class_='card card-hover card-visited wordwrap job-link')
 		for index, iterator in enumerate(ads):
@@ -88,7 +89,7 @@ class WorkUaParser(Parser):
 			except Exception as e:
 				print(e)
 			try:
-				page = await asyncio.ensure_future(self.get_html(url))
+				page = await self.get_html(url)
 				desc_soup = BeautifulSoup(page, 'lxml')
 				company = desc_soup.find('dl', class_='dl-horizontal').find('a').find('b').get_text().strip()
 				city = desc_soup.find('dl', class_='dl-horizontal').findAll('dd')[-2].get_text().strip()
@@ -117,8 +118,7 @@ class WorkUaParser(Parser):
 															
 			}
 			
-			await asyncio.ensure_future(self.write_csv(data=data, message=str(self.message), fileprefix=str(self.chat_id)))
-
+			await self.write_csv(data=data, message=str(self.message), fileprefix=str(self.chat_id))
 
 
 def read_file(filename):
@@ -132,8 +132,8 @@ def main(useragent):
 	p = WorkUaParser(url='https://www.work.ua/jobs-', page='page=', message='javascript', chat_id='121212121')
 	loop = asyncio.new_event_loop()
 	asyncio.set_event_loop(loop)
-	r = loop.run_until_complete(p.workparse(useragent))
-	loop.close()
+	r = loop.run_until_complete(asyncio.gather(p.start_parsing(useragent)))
+	# loop.close()
 	
 if __name__ == '__main__':
 	main(useragent)
